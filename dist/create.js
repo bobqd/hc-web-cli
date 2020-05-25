66b3761daa122f5d9ab0517c543242750dfa625c
bob_web <962563175@qq.com>
2020-05-25 11:17:41 +0800

    添加react模板

--- dist/create.js
+++ dist/create.js
@@ -28,16 +28,8 @@ let create = async ProjectName => {
     } else {
         // 如果文件名不存在则继续执行,否则退出
         (0, _util.notExistFold)(ProjectName).then(() => {
-
             // 用户询问交互
             (0, _util.prompt)().then(answer => {
-
-                // 目前只建了一个vue的模板，所以只能先跳过react🌶 
-                if (answer.frame === 'react') {
-                    console.log(_logSymbols2.default.warning, _chalk2.default.yellow('react模板还在路上，莫急莫急~'));
-                    process.exit(1);
-                }
-
                 /**
                  * 根据用户输入的配置信息下载模版&更新模版配置
                  * 下载模版比较耗时,这里通过ora插入下载loading, 提示用户正在下载模版
@@ -58,33 +50,12 @@ let create = async ProjectName => {
                 }
 
                 (0, _util.downloadTemplate)(ProjectName, Api).then(async () => {
-
                     loading.succeed('模板下载完成');
-
                     // 下载完成后,根据用户输入更新配置文件
                     const fileName = `${ProjectName}/package.json`;
                     answer.name = ProjectName;
                     answer.dependencies = {};
-                    switch (answer.library) {
-                        case 'ElementUI':
-                            answer.dependencies['element-ui'] = "^2.13.0";
-                            break;
-                        case 'iView':
-                            answer.dependencies['iview'] = "^3.5.0";
-                            break;
-                        case 'Vant':
-                            answer.dependencies['vant'] = "^2.8.0";
-                            answer.dependencies['lib-flexible'] = "^0.3.0";
-                            answer.dependencies['postcss-px2rem'] = "^0.3.0";
-                            break;
-                        case 'Minui':
-                            answer.dependencies['mint-ui'] = "^7.0.0";
-                            answer.dependencies['lib-flexible'] = "^0.3.0";
-                            answer.dependencies['postcss-px2rem'] = "^0.3.0";
-                            break;
-                        default:
-                            break;
-                    }
+
                     switch (answer.request) {
                         case 'axios':
                             answer.dependencies['axios'] = "^0.19.0";
@@ -95,23 +66,70 @@ let create = async ProjectName => {
                         default:
                             break;
                     }
-                    (0, _util.updateJsonFile)(fileName, answer).then(() => {
-                        console.log(_logSymbols2.default.success, _chalk2.default.green('配置已完成，cd到项目目录，执行npm install'));
-                    });
 
-                    //如果选择移动端，需要修改main.js中引入lib-flexible,添加px2rem配置
-                    if (answer.client == 'Mobile') {
-                        if (_fs2.default.existsSync(`${ProjectName}/src/main.js`)) {
-                            const data = _fs2.default.readFileSync(`${ProjectName}/src/main.js`, 'utf8').split('\n');
-                            data.splice(0, 0, "import 'lib-flexible/flexible.js'");
-                            _fs2.default.writeFileSync(`${ProjectName}/src/main.js`, data.join('\n'), 'utf8');
+                    if (answer.frame === 'vue') {
+                        switch (answer.library) {
+                            case 'ElementUI':
+                                answer.dependencies['element-ui'] = "^2.13.0";
+                                break;
+                            case 'iView':
+                                answer.dependencies['iview'] = "^3.5.0";
+                                break;
+                            case 'Vant':
+                                answer.dependencies['vant'] = "^2.8.0";
+                                answer.dependencies['lib-flexible'] = "^0.3.0";
+                                answer.dependencies['postcss-px2rem'] = "^0.3.0";
+                                break;
+                            case 'Minui':
+                                answer.dependencies['mint-ui'] = "^7.0.0";
+                                answer.dependencies['lib-flexible'] = "^0.3.0";
+                                answer.dependencies['postcss-px2rem'] = "^0.3.0";
+                                break;
+                            default:
+                                break;
                         }
-                        if (_fs2.default.existsSync(`${ProjectName}/vue.config.js`)) {
-                            const vueConfig = _fs2.default.readFileSync(`${ProjectName}/vue.config.js`, 'utf8').split('\n'),
-                                  px2rem = '  css: {\n' + '    loaderOptions: {\n' + '      postcss: {\n' + '        plugins: [\n' + '          require("postcss-px2rem")({\n' + '            remUnit: 75\n' + '          })\n' + '        ]\n' + '      }\n' + '    }\n' + '  },';
-                            vueConfig.splice(1, 0, px2rem);
-                            _fs2.default.writeFileSync(`${ProjectName}/vue.config.js`, vueConfig.join('\n'), 'utf8');
+                        (0, _util.updateJsonFile)(fileName, answer).then(() => {
+                            console.log(_logSymbols2.default.success, _chalk2.default.green(`配置已完成，cd ${ProjectName}，执行npm install`));
+                        });
+                        //如果选择移动端，需要修改main.js中引入lib-flexible,添加px2rem配置
+                        if (answer.client == 'Mobile') {
+                            if (_fs2.default.existsSync(`${ProjectName}/src/main.js`)) {
+                                const data = _fs2.default.readFileSync(`${ProjectName}/src/main.js`, 'utf8').split('\n');
+                                data.splice(0, 0, "import 'lib-flexible/flexible.js'");
+                                _fs2.default.writeFileSync(`${ProjectName}/src/main.js`, data.join('\n'), 'utf8');
+                            }
+                            if (_fs2.default.existsSync(`${ProjectName}/vue.config.js`)) {
+                                const vueConfig = _fs2.default.readFileSync(`${ProjectName}/vue.config.js`, 'utf8').split('\n'),
+                                      px2rem = '  css: {\n' + '    loaderOptions: {\n' + '      postcss: {\n' + '        plugins: [\n' + '          require("postcss-px2rem")({\n' + '            remUnit: 75\n' + '          })\n' + '        ]\n' + '      }\n' + '    }\n' + '  },';
+                                vueConfig.splice(1, 0, px2rem);
+                                _fs2.default.writeFileSync(`${ProjectName}/vue.config.js`, vueConfig.join('\n'), 'utf8');
+                            }
                         }
+                    } else if (answer.frame === 'react') {
+                        switch (answer.library) {
+                            case 'amaze-ui':
+                                answer.dependencies['amazeui'] = "^2.7.0";
+                                break;
+                            case 'ant-design':
+                                answer.dependencies['antd'] = "^4.2.0";
+                                break;
+                            case 'amaze-ui-touch':
+                                answer.dependencies['amazeui-touch'] = "^2.0.0";
+                                answer.dependencies['lib-flexible'] = "^0.3.0";
+                                answer.dependencies['postcss-px2rem'] = "^0.3.0";
+                                break;
+                            case 'ant-design-mobile':
+                                answer.dependencies['antd-mobile'] = "^2.3.0";
+                                answer.dependencies['lib-flexible'] = "^0.3.0";
+                                answer.dependencies['postcss-px2rem'] = "^0.3.0";
+                                break;
+                            default:
+                                break;
+                        }
+
+                        (0, _util.updateJsonFile)(fileName, answer).then(() => {
+                            console.log(_logSymbols2.default.success, _chalk2.default.green(`配置已完成，cd ${ProjectName}，执行npm install`));
+                        });
                     }
                 }, () => {
                     loading.fail('模板下载失败');
--- dist/util.js
+++ dist/util.js
@@ -50,7 +50,7 @@ let promptList = [{
     message: 'Please choose the UI library name: ',
     choices: ['ElementUI', 'iView'],
     when: function (answers) {
-        return answers.client == 'PC';
+        return answers.client == 'PC' && answers.frame == 'vue';
     }
 }, {
     type: 'list',
@@ -58,7 +58,23 @@ let promptList = [{
     message: 'Please choose the UI library name: ',
     choices: ['Vant', 'Minui'],
     when: function (answers) {
-        return answers.client == 'Mobile';
+        return answers.client == 'Mobile' && answers.frame == 'vue';
+    }
+}, {
+    type: 'list',
+    name: 'library',
+    message: 'Please choose the UI library name: ',
+    choices: ['amaze-ui', 'ant-design'],
+    when: function (answers) {
+        return answers.client == 'PC' && answers.frame == 'react';
+    }
+}, {
+    type: 'list',
+    name: 'library',
+    message: 'Please choose the UI library name: ',
+    choices: ['amaze-ui-touch', 'ant-design-mobile'],
+    when: function (answers) {
+        return answers.client == 'Mobile' && answers.frame == 'react';
     }
 }, {
     type: 'list',
